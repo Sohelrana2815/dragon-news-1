cf9d234b7235d28f580faa059a5c1e371971b6cf
Sohelrana2815 <sohel152302@gmail.com>
2024-06-02 07:20:23 +0600

    over all page section divided

---
+++ src/Pages/Shared/Navbar.jsx
@@ -0,0 +1,11 @@
+
+const Navbar = () => {
+  return (
+    <div>
+      <h2 className="text4xl">Navbar</h2>
+      
+    </div>
+  );
+};
+
+export default Navbar;
--- src/Routes/layouts/Root.jsx
+++ src/Routes/layouts/Root.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const Root = () => {
   return (
-    <div>
+    <div className="lg:max-w-[1440px] mx-auto font-poppins">
       <Outlet></Outlet>
     </div>
   );
