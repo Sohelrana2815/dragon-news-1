00cc2a7c44facebf7905b2876feeadffb2da3e67
Sohelrana2815 <sohel152302@gmail.com>
2024-06-03 07:39:09 +0600

    first commit

--- src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,54 +1,67 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import News from "./news";
+import { CiCalendar } from "react-icons/ci";
 
 const LeftSideNav = () => {
   // all news data load
 
   const [categories, setCategories] = useState([]);
 
+  const [newsData, setNewsData] = useState([]);
+
   useEffect(() => {
     fetch("categories.json")
       .then((res) => res.json())
       .then((data) => setCategories(data));
   }, []);
+  useEffect(() => {
+    fetch("news.json")
+      .then((res) => res.json())
+      .then((data) => setNewsData(data));
+  }, []);
 
   return (
-    <div className="space-y-8 p-4">
-      <h2 className="text-3xl">All Category</h2>
-      {categories.map((category) => (
-        <>
-          <Link
-            to={`/category/${category.id}`}
-            className="block text-xl font-semibold ml-4"
-            key={category.id}
-          >
-            {category.name}
-          </Link>
-        </>
-      ))}
+    <>
+      <div className="space-y-8 p-4">
+        <h2 className="text-3xl">All Category</h2>
+        {categories.map((category, idx) => (
+          <>
+            <Link
+              to={`/category/${category.id}`}
+              className="block text-xl font-semibold ml-4"
+              key={idx}
+            >
+              {category.name}
+            </Link>
+          </>
+        ))}
 
-      <div>
-        <h2 className="text-2xl">All News</h2>
-        {/*left news card */}
-        <div className="card bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body">
-            <p>Bayern Slams Authorities Over Flight Delay to Club World Cup</p>
-            <div className="card-actions space-x-10">
-              <div>Fashion</div>
-              <div>Products</div>
-            </div>
-          </div>
+        <div className="space-y-10">
+          {/*left news card */}
+          <h2 className="text-2xl mt-20">All News</h2>
+          {newsData.map((news) => (
+            <>
+              <div className="card bg-base-100 shadow-xl">
+                <figure>
+                  <img src={news.author.img} />
+                </figure>
+                <div className="card-body">
+                  <p>{news.title}</p>
+                  <div className=" gap-6 space-y-2">
+                    <p className="text-base w-full">{news.author.name}</p>
+
+                    <p className="text-sm flex items-center">
+                      <CiCalendar className="text-2xl mr-2"></CiCalendar>
+                      {news.author.published_date}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </>
+          ))}
         </div>
       </div>
-      <News></News>
-    </div>
+    </>
   );
 };
 
